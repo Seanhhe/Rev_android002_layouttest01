93b93ab53f7d99103e7d60f1e4bc010ecb6ed3aa
Seanhhe <seanhhe@gmail.com>
2020-07-08 16:36:51 +0800

    2020-07-08_android_LinearLayout

--- app/src/main/java/com/example/rev_android002_layouttest01/MainActivity.java
+++ app/src/main/java/com/example/rev_android002_layouttest01/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.rev_android002_layouttest01;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
